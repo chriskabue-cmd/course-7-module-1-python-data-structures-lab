44b5a48ee1bbc832778851fa8b756a8602e31cc1
Chris kabue <chris.kabue@student.moringaschool.com>
2026-03-03 10:56:05 +0300

    completed course-7-module-1-python-data-structures-lab

--- lib/data_processing.py
+++ lib/data_processing.py
@@ -9,11 +9,15 @@ def format_student_data(student):
     - Major
     such as: "ID: 10 | Name: Louis Medina | Major: Computer Science"
     """
-    pass
+    student_id, name, major = student
+    return f"ID: {student_id} | Name: {name} | Major: {major}"
+pass
 
 def display_students(student_list):
     """
     Display all student records.
     Loop through the student_list and print each student using format_student_data().
     """
+    for student in student_list:
+        print(format_student_data(student))
     pass
--- lib/student_data.py
+++ lib/student_data.py
@@ -1,6 +1,12 @@
 # This module initializes student records.
 
 # Define a list of students stored as tuples (ID, Name, Major)
+# List of students stored as tuples (ID, Name, Major)
 students = [
-
+    (120, "Lisa Bossy", "Home Science"),
+    (121, "Boaz Limuel", "Mathematics"),
+    (122, "Johnson Ndile", "Physics"),
+    (124, "Jane Wanja", "Chemistry"),
+    (125, "Unique Kabue", "Biology"),
 ]
+
